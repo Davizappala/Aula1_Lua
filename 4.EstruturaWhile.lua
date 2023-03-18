b37c65f4f88fdd3beb3be73d2bb2d30313d5a545
Davi Zappala <94610458+Davizappala@users.noreply.github.com>
2023-03-18 15:46:38 -0300

    Updating Files

---
+++ 4.EstruturaWhile.lua
@@ -0,0 +1,10 @@
+-- While
+
+local energia = 10
+
+while (energia > 0) do 
+  print ("Você esta vivo com energia = : " .. energia) 
+  energia = energia - 1
+end
+
+print("Morto!")
---
+++ 5.EstruturaFor.lua
@@ -0,0 +1,6 @@
+-- For
+for i=10,1,-1
+do
+  print ("Renascimento em :" ..i)
+end
+print("Renasceu")
